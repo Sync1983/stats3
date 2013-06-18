a323c83e5f530f2e1c88e6c9f8f127e2cd3670c5
sync1983 <sync06@mail.ru>
2013-06-18 11:17:19 +0400

    server commit

---
+++ cron/clearOldRecords.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(__DIR__.'/../setup.php');
+
+$host = lmb_env_get('DB_HOST');
+$user = lmb_env_get('DB_USER');
+$pass = lmb_env_get('DB_PASS');
+          
+main();
+exit(0);
+
+function main() {
+  $host = lmb_env_get('DB_HOST');
+  $user = lmb_env_get('DB_USER');
+  $pass = lmb_env_get('DB_PASS');
+  $depricate = array('log_Pay','log_NewPlayer','logger','logger_chart','log_MAU');
+
+  $db = new mysqli($host,$user,$pass,'stats2');
+  $tables_db = $db->query("SHOW TABLES LIKE 'log_%'");
+  $tables = array();
+  while($row = $tables_db->fetch_array()) {
+    if(!in_array($row[0],$depricate))
+      $tables[] = $row[0];
+  }
+  print_r($tables);
+  $time = time() - 65*86400;
+  foreach($tables as $table) {
+    $SQL = "DELETE FROM $table where stamp<$time";
+    echo "SQL: $SQL\r\n";
+    $result = $db->query($SQL);
+    var_dump($result);
+  }
+}
