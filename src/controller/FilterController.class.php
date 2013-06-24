e8cdd93d95061425f23b51f2a093c564f6e361a1
= <sync06@mail.ru>
2013-06-24 16:44:51 +0400

    change filters

--- src/controller/FilterController.class.php
+++ src/controller/FilterController.class.php
@@ -24,12 +24,28 @@ class FilterController extends spController {
     $request = $this->toolkit->request;
     $project_id = $request['project_id'];
     $name = $request['name'];
-    $data = json_encode($request['filter']);
-    $filter = new Filter();
-    $filter->set('project_id',$project_id);
-    $filter->set('name',$name);
-    $filter->set('data',$data);
-    $filter->save();
-    $this->sendAjaxSuccess();
+    $data = json_encode($request['filter']);    
+    //$find = lmbActiveRecord::findOne("Filter", array('name'=>$name,'project_id'=>$project_id),FALSE);        
+    $find = new Filter();
+    /** @var mysqli **/
+    $db  = $find->getDefaultConnection();
+    $SQL = "SELECT id FROM filter where `project_id`=$project_id and `name`='$name' LIMIT 0,1";
+    $result = $db->execute($SQL);
+    $row = $result->fetch_assoc();
+    if(!$row) {
+      $find->set('project_id',$project_id);
+      $find->set('name',$name);    
+      $find->set('data',$data);
+      $find->save();    
+    } else {
+      $SQL = "UPDATE filter SET `data`='$data' WHERE id=".$row['id'];
+      $db->execute($SQL);
+    }      
+    $SQL = "SELECT * FROM filter where `project_id`=$project_id";    
+    $result = $db->execute($SQL);
+    $return = array();
+    while($row = $result->fetch_assoc())
+      $return[]=$row;
+    $this->sendAjaxResponce($return);
   }
 }
--- www/js/filter.js
+++ www/js/filter.js
@@ -2,29 +2,30 @@ function filter_funct(parent) {
   
   var activeParams = {};
   
-  function setupFilter() {
-    var body = $('#filter > div.modal-body').eq(0).children('#filter-param').eq(0).children('tbody').eq(0);    
+  function setupFilter() {    
+    var filter = $("#filter").children('div.modal-body').eq(0).children('#filter-param').eq(0).children('tbody').eq(0);    
+    for(var i in activeParams) {
+      var param = activeParams[i];
+      this.addRule(param.item,param.operation,param.value,param.logic);
+    }
+    var name = $("#filter-button").text();
+    name = $.trim(name.replace("Фильтр: ",''));
+    if(name!=="Все")
+      $("#filter").children('div.modal-header').eq(0).children('input').val(name);
     return;
   }
   
-  function constructParams(form){
-    var filter = $(form).parent().parent();
-    filter = $(filter).children('div.modal-body').eq(0).children('#filter-param').eq(0).children('tbody').eq(0);
-    var rows = filter.children('tr');
+  function constructParams(){    
+    var rows = $("#filter").children('div.modal-body').children('table').children('tbody').children('tr');    
     var result = new Array();      
-    for(i=0;i<rows.length;i+=2){
-      var row = rows[i];      
-      var items = $(row).children('td');
-      if(items.length<=0)
-        continue;
-      var item = $(items).children('[name*="row_name"]').children('button[name*="btn_name"]').attr('btn_id');
-      var op = $(items).children('[name*="row_op"]').children('button[name*="btn_name"]').attr('btn_id');
-      var value = $(items).children('input').val();
-      var logic = $(rows[i+1]).children('td').children('[name*="row_and"]').children('button[name*="btn_name"]').attr('btn_id');
-      if( (!item)||(item==="-1") || (!op)||(op==="-1") || (value==="-"))
-        continue;      
-      var rule = {item:item,operation:op,value:value};
-      if(logic)
+    for(i=0;i<rows.length;i++){
+      var row = rows[i];   
+      var name  = $(row).children('td').children('div[name*="row_name"]').children('button[name*="btn_name"]').attr('btn_id');
+      var oper  = $(row).children('td').children('div[name*="row_op"]').  children('button[name*="btn_name"]').attr('btn_id');
+      var value = $(row).children('td').children('input').val();
+      var logic = $(row).children('td').children('div[name*="row_and"]'). children('button[name*="btn_name"]').attr('btn_id');      
+      var rule = {item:name,operation:oper,value:value};
+      if(i+1<rows.length)
         rule['logic'] = logic;
       result.push(rule);
     }
@@ -32,19 +33,16 @@ function filter_funct(parent) {
     return result;
   }
 
+  function isEmpty(obj) {
+    for (var key in obj)
+      return false;
+    return true;
+  }
+
   /* ======================================= Public ===========================================*/
   
-  this.addRule = function(item) {
-    var parent = $(item).parent().parent().parent();
-    var button = "<div class=\"btn-group\" name=\"row_and\">"
-        +"  <button name =\"btn_name\" btn_id =\"AND\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\">AND</button>"
-        +"  <button class=\"btn dropdown-toggle\" data-toggle=\"dropdown\"><span class=\"caret\"></span></button>"
-        +"  <ul class=\"dropdown-menu\">"
-        +"    <li><a href=\"AND\" btn_id=\"AND\"  onclick=\"return window.filter.changeColumn(this);\">AND</a></li>"
-        +"    <li><a href=\"OR\" btn_id=\"OR\"    onclick=\"return window.filter.changeColumn(this);\">OR</a></li>"
-        +"  </ul>"
-        +"</div>";  
-    var html = "<tr><td>"+
+  this.addRule = function(i_name,i_operation,i_value,i_logic) {    
+    var item = 
       "<div class=\"btn-group\" name=\"row_name\">"+
       "  <button name =\"btn_name\" btn_id =\"-1\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" style=\"width:100px;\">Поле</button>"+
       "  <button class=\"btn dropdown-toggle\" data-toggle=\"dropdown\"><span class=\"caret\"></span></button>"+           
@@ -58,7 +56,8 @@ function filter_funct(parent) {
       "    <li><a href=\"#\" btn_id=\"referal\"  onclick=\"return window.filter.changeColumn(this);\">Реферал           </a></li>"+
       "    <li><a href=\"#\" btn_id=\"reg_time\" onclick=\"return window.filter.changeColumn(this);\">Дата регистрации  </a></li>"+         
       "  </ul>"+
-      "</div>"+
+      "</div>";    
+    var operation = 
       "<div class=\"btn-group\" name=\"row_op\">"+
       "  <button name =\"btn_name\" btn_id =\"-1\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\" style=\"width:80px;\">Действие</button>"+
       "  <button class=\"btn dropdown-toggle\" data-toggle=\"dropdown\"><span class=\"caret\"></span></button>"+
@@ -71,23 +70,70 @@ function filter_funct(parent) {
       "    <li><a href=\"#\" btn_id=\"IN\"    onclick=\"return window.filter.changeColumn(this);\">IN</a></li>"+
       "    <li><a href=\"#\" btn_id=\"LIKE\"  onclick=\"return window.filter.changeColumn(this);\">LIKE</a></li>"+
       "  </ul>"+
-      "</div>"+
-      "  <input class=\"input-medium\" type=\"text\" value=\"-\" style=\"margin-top: 10px;width: 380px;\"/>"+
-      "  <button class =\"btn\"><span class=\"icon-remove\" onclick=\"return window.filter.removeRule(this);\"></span></button>"+
-      "</td></tr>"+
-      "<tr>"+
-      "  <td>"+
-      "  <input type=\"button\" class=\"btn btn-primary\" value=\"Добавить строку\" onclick=\"return window.filter.addRule(this);\"/>"+            
-      "  </td>"+
-      "</tr>";    
-    $(parent).append(html);
-    $(item).parent().html(button);
+      "</div>";
+    var value = 
+      "  <input class=\"input-medium\" type=\"text\" value=\"-\" style=\"margin-top: 10px;width: 380px;\"/>";
+    var logic = 
+         "<div class=\"btn-group\" name=\"row_and\">"
+        +"  <button name =\"btn_name\" btn_id =\"AND\" class=\"btn dropdown-toggle\" data-toggle=\"dropdown\">AND</button>"
+        +"  <button class=\"btn dropdown-toggle\" data-toggle=\"dropdown\"><span class=\"caret\"></span></button>"
+        +"  <ul class=\"dropdown-menu\">"
+        +"    <li><a href=\"AND\" btn_id=\"AND\"  onclick=\"return window.filter.changeColumn(this);\">AND</a></li>"
+        +"    <li><a href=\"OR\" btn_id=\"OR\"    onclick=\"return window.filter.changeColumn(this);\">OR</a></li>"
+        +"  </ul>"
+        +"</div>";    
+    var del =   
+      "  <button class =\"btn\"><span class=\"icon-remove\" onclick=\"return window.filter.removeRule(this);\"></span></button>";
+    var string = 
+       "<tr>"
+      +"  <td>"
+      +   item
+      +"  </td>"
+      +"  <td>"
+      +   operation
+      +"  </td>"
+      +"  <td>"
+      +   value
+      +"  </td>"
+      +"  <td>"
+      +   logic
+      +"  </td>"
+      +"  <td>"
+      +   del
+      +"  </td>"
+      +"</tr>";
+    var res = $(string);
+    $("#filter").children('div.modal-body').children('table').append(res);
+    var e_name  = $(res).children('td').children('div[name*="row_name"]').children('ul');
+    var e_oper  = $(res).children('td').children('div[name*="row_op"]').  children('ul');
+    var e_value = $(res).children('td').children('input');
+    var e_logic = $(res).children('td').children('div[name*="row_and"]'). children('ul');
+    
+    function setValue(target,value) {
+      var items = $(target).children('li');
+      for(var i =0;i<=items.length;i++) {
+        var ref = $(items[i]).children('a');
+        if(ref.attr("btn_id")===value){
+          this.changeColumn(ref);
+          break;
+        }          
+      }
+      return;
+    }   
+  
+    if(i_value!==null)
+      $(e_value).val(i_value);
+    if(i_logic!==null)
+      setValue(e_logic,i_logic);
+    if(i_name!==null)
+      setValue(e_name,i_name);
+    if(i_operation!==null)
+      setValue(e_oper,i_operation);    
     return false;
   };
 
   this.removeRule = function (item) {
     var parent = $(item).parent().parent().parent();    
-    $(parent).prev('tr').remove();
     $(parent).remove();
     return false;
   };
@@ -106,17 +152,20 @@ function filter_funct(parent) {
   };
 
   this.show = function() {    
-    function onAnswer(data) {      
-      data = JSON.parse(data);      
-      if(data.html) {
-        $('#filter > div.modal-body').html(data.html);        
-        $('#filter').modal({show:true});
-        return;
-      } else
-        console.log(data);
-    }
-    main.ajax('filter','ajax_load_constructor',{project_id:project_id, page_id:main.selectedTabId}, onAnswer);
-    return false;
+   var add_button = 
+    +"<tr>"
+    +"  <td colspan=4>"
+    +"    <input type=\"button\" class=\"btn btn-primary\" value=\"Добавить строку\" onclick=\"return window.filter.addRule();\"/>"
+    +"  </td>"     
+    +"</tr>";
+   $('#filter > div.modal-body').children('table').html(add_button);
+   $('#filter').modal({show:true}); 
+   
+   if(isEmpty(activeParams))
+    this.addRule(1,2,"qwert","OR");
+   else
+    setupFilter();
+   return false;
   };
 
   this.setup = function(data,item) {  
@@ -128,7 +177,7 @@ function filter_funct(parent) {
   };
 
   this.apply = function(form) {
-    var params = constructParams(form);
+    var params = constructParams(form);    
     activeParams = params;  
     window.main.pageReload();
     $(form).parent().parent().modal('hide');
@@ -142,8 +191,33 @@ function filter_funct(parent) {
     if(name=="") {
       $(form).addClass('disabled');
       return false;
-    }  
-    main.ajax('filter','ajax_save_filter',{project_id:project_id, filter:params, name: name});
+    };
+  
+    function onSaved(data) {
+      data = JSON.parse(data);
+      console.log(data);
+      $("#filter-dropdown").html('');
+      var html = '';
+      for(var i in data) {
+        var item = 
+           "<li>"
+          +"  <a href=\"#\" onclick='return window.filter.setup("+data[i]['data']+",this);'>"
+          +"    <input type=\"radio\" name=\"optionsRadios\" id=\""+data[i]['name']+"\" value=\""+data[i]['name']+"\" style=\"margin-right: 10px;margin-top: -2px;\"/>"+data[i]['name']
+          +"  </a>"
+          +"</li>";
+        html += item;
+      }
+      var fin = 
+         "<li>"
+        +"  <a href=\"#\" onclick=\"return window.filter.setup({},this);\">"
+        +"    <input type=\"radio\" name=\"optionsRadios\" id=\"all\" value=\"all\" style=\"margin-right: 10px;margin-top: -2px;\"/>Все"
+        +"  </a>"
+        +"</li>"
+        +"<li><a href=\"\" role=\"button\" class=\"btn\" data-toggle=\"modal\" onclick=\"return window.filter.show();\">Изменить</a></li>";
+      $("#filter-dropdown").html(html+fin);
+    };
+  
+    main.ajax('filter','ajax_save_filter',{project_id:project_id, filter:params, name: name},onSaved);
     activeParams = params;  
     window.main.pageReload();  
     $(form).parent().parent().modal('hide');
